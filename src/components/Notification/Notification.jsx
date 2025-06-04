ad95c4375e0c4fc903ddd35871c43c35756c3eb8
Oksana <kursorksu777@gmail.com>
2025-06-04 21:48:49 +0200

    lesson for mobile

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -5,34 +5,28 @@ import { deleteMessage } from '../../store/notificationReducer';
 import clsx from 'clsx';
 
 export const Notification = () => {
-  const notificationText = useSelector((state) => state.notification);
-  const dispatch = useDispatch();
-  const duration = 10000;
+    const notificationText = useSelector((state) => state.notification);
+    const dispatch = useDispatch();
+    const duration = 10000;
     const [api, contextHolder] = notification.useNotification();
 
-    const openNotification = (pauseOnHover) => () => {
-        api.open({
-            type: notificationText?.type,
-            message: notificationText?.message.title,
-            description: notificationText?.message.description,
-            showProgress: true,
-            pauseOnHover,
-        });
-    };
-
-
     useEffect(() => {
-        let timout = setTimeout(() => {
-            dispatch(deleteMessage());
-            openNotification(true)();
-        }, duration);
+        if (notificationText?.type) {
+            api.open({
+                type: notificationText.type,
+                message: notificationText.message.title,
+                description: notificationText.message.description,
+                duration: duration / 1000,
+                onClose: () => {
+                    dispatch(deleteMessage());
+                },
+            });
+        }
+    }, [notificationText, api, dispatch, duration]);
 
-        return () => clearTimeout(timout);
-    }, [dispatch, openNotification]);
-
-  return (
-      <div  className={clsx(notificationText?.type, { open: notificationText?.type })}>
-          {contextHolder}
-      </div>
-  );
+    return notificationText?.type ? (
+        <div className={clsx(notificationText.type, { open: notificationText.type })}>
+            {contextHolder}
+        </div>
+    ) : null;
 };
